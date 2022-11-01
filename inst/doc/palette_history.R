ef667be7acc8f6988d2284ce3ded72522b8e01f4
Laura Bakala <erdaradun.gaztea@gmail.com>
2022-11-01 09:40:16 +0000

    version 0.1.1

---
+++ inst/doc/palette_history.R
@@ -0,0 +1,10 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  fig.height = 1.6,
+  fig.width = 5
+)
+library(gglgbtq)
+options(gglgbtq_font_size = 1.7)
+
